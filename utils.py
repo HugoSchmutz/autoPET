b2acc89584cd331e40774c6b119929b78cfc39c3
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-06-29 11:18:59 +0200

    fix bug SegPL

--- utils.py
+++ utils.py
@@ -74,9 +74,9 @@ def train_epoch_SegPL(model, optimizer, loss_function, loader, loader_ulb, lmbd,
         print((logits_u>0).shape)
         
         print((logits_u>0)[:,1:].detach().shape)
-        print(torch.unbind((logits_u>0)[:,1].detach())[0].shape)
+        print(torch.unbind((logits_u>0)[:,1:].detach())[0].shape)
 
-        pseudo_labels =  torch.stack([tio.OneHot(num_classes=2)(m) for m in torch.unbind((logits_u>0)[:,1].detach(), dim=0) ], dim=0)
+        pseudo_labels =  torch.stack([tio.OneHot(num_classes=2)(m) for m in torch.unbind((logits_u>0)[:,1:].detach(), dim=0) ], dim=0)
         
         unsup_loss = loss_function(logits_u, pseudo_labels)
         
