297a19bde6f1c495bb70513fe4e36ae66a671c45
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-07-20 09:56:07 +0200

    new training method

--- SegPL.sh
+++ SegPL.sh
@@ -1,4 +1,4 @@
 module load conda/5.0.1-python3.6
 source activate virt_pytorch_conda
 
-python new_train.py --data_path /data/maasai/user/hschmutz/ --SegPL
+python new_train.py --data_dirS /data/maasai/user/hschmutz/ --SegPL
