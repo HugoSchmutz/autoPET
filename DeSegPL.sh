bc1b6d33a3ddc08c235dc887fed760be87cb33bc
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-08-07 11:09:34 +0200

    test_script to test trained model

---
+++ DeSegPL.sh
@@ -0,0 +1,4 @@
+module load conda/5.0.1-python3.6
+source activate virt_pytorch_conda
+
+python new_train.py --data_dir /data/maasai/user/hschmutz/ --overwrite --rank 0 --gpu 0 --SegPL --ulb_loss_ratio 0.005 --debiased
