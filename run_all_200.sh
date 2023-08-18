032a6f6ead409fce9f45265e701e4f614037e971
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-08-18 19:25:20 +0200

    modify name save

--- run_all_200.sh
+++ run_all_200.sh
@@ -1,11 +1,11 @@
 #Pretraining
-python3 new_train.py --data_dir data/FDG-PET-CT-Lesions_nifti --overwrite --rank 0 --gpu 1 --ulb_loss_ratio 0.0
+#python3 new_train.py --data_dir data/FDG-PET-CT-Lesions_nifti --overwrite --rank 0 --gpu 1 --ulb_loss_ratio 0.0
 #SegPL
-python3 new_train.py --data_dir data/FDG-PET-CT-Lesions_nifti --overwrite --rank 0 --gpu 1 --finetune --load_path saved_models/CC_200_0.0_0/model_best.pth --ulb_loss_ratio 0.1 --SegPL
-python3 test_script.py --load_path ./saved_models/FT_SegPL_200_0.1_0/
+#python3 new_train.py --data_dir data/FDG-PET-CT-Lesions_nifti --overwrite --rank 0 --gpu 1 --finetune --load_path saved_models/CC_200_0.0_0/model_best.pth --ulb_loss_ratio 0.1 --SegPL
+#python3 test_script.py --load_path ./saved_models/FT_SegPL_200_0.1_0/
 #DeSegPL
-python3 new_train.py --data_dir data/FDG-PET-CT-Lesions_nifti --overwrite --rank 0 --gpu 1 --finetune --load_path saved_models/CC_200_0.0_0/model_best.pth --ulb_loss_ratio 0.1 --SegPL --debiased
-python3 test_script.py --load_path ./saved_models/FT_DeSegPL_200_0.1_0/
+#python3 new_train.py --data_dir data/FDG-PET-CT-Lesions_nifti --overwrite --rank 0 --gpu 1 --finetune --load_path saved_models/CC_200_0.0_0/model_best.pth --ulb_loss_ratio 0.1 --SegPL --debiased
+#python3 test_script.py --load_path ./saved_models/FT_DeSegPL_200_0.1_0/
 #SegPL_U
 python3 new_train.py --data_dir data/FDG-PET-CT-Lesions_nifti --overwrite --rank 0 --gpu 1 --finetune --load_path saved_models/CC_200_0.0_0/model_best.pth --ulb_loss_ratio 0.1 --SegPL_U --ulb_loss_fct maskedCE
 python3 test_script.py --load_path ./saved_models/FT_SegPL_U_200_0.1_0/
