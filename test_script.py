0a9d1df405f0c1b0cc6b7fd0e20465839a96412f
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-10-05 16:08:48 +0200

    MC dropout test

--- test_script.py
+++ test_script.py
@@ -78,10 +78,11 @@ if __name__ == "__main__":
         for i, data in enumerate(tqdm(test_loader)):
             roi_size = (128, 128, 32)
             sw_batch_size = 40
-            
+            print(data['pet'][tio.DATA])
+            print(data['ct'][tio.DATA])
             X, y = prepare_batch(data, args.gpu)
             print(X.shape)
-            '''
+
             mean_logits = torch.zeros(X.shape).cuda(args.gpu)
             for i in range(T_eval):
                 mean_logits = +sliding_window_inference(X, roi_size, sw_batch_size, net, mode="gaussian", overlap=0.50)
@@ -153,4 +154,3 @@ if __name__ == "__main__":
     plt.savefig(os.path.join(args.load_path,'nb_lesions.pdf'), format = 'pdf')
     print(np.corrcoef(res['true_nb_lesions'].values, res['pred_nb_lesions'].values))
     
-    '''
