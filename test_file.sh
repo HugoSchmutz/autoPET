622a380923e62423a27ff254b923d14545918cf7
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-10-22 13:04:49 +0200

    add test file

--- test_file.sh
+++ test_file.sh
@@ -36,8 +36,8 @@ python3 test_script.py --load_path ./saved_models/FT_CC_200_0.0_0/
 
 #SegMT_softmax
 python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_U_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_MT_softmax_normal_maskedCE_50_0.1_0_0.0/
+python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_U_50_0.1_0/
 
 #DeSegMT_softmax
 python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_DeMT_softmax_normal_maskedCE_50_0.1_0_0.0/
+python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_50_0.1_0/
