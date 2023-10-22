0470aff07e6ffad48b6454401f3bf8ce94cdc88c
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-10-22 13:03:16 +0200

    add test file

--- test_file.sh
+++ test_file.sh
@@ -1,38 +1,38 @@
 #Pretraining
-python3 test_script.py --load_path ./saved_models/CC_200_pretraining/
-python3 test_script.py --load_path ./saved_models/CC_50_pretraining/
+#python3 test_script.py --load_path ./saved_models/CC_200_pretraining/
+#python3 test_script.py --load_path ./saved_models/CC_50_pretraining/
 
 #CC
 python3 test_script.py --load_path ./saved_models/FT_CC_200_0.0_0/
-python3 test_script.py --load_path ./saved_models/FT_CC_50_0.0_0/
+#python3 test_script.py --load_path ./saved_models/FT_CC_50_0.0_0/
 
 #SegPL
-python3 test_script.py --load_path ./saved_models/FT_SegPL_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_SegPL_50_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_SegPL_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_SegPL_50_0.1_0/
 
 #DeSegPL
-python3 test_script.py --load_path ./saved_models/FT_DeSegPL_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_DeSegPL_50_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_DeSegPL_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_DeSegPL_50_0.1_0/
 
 #SegMT
-python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_50_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_50_0.1_0/
 
 
 #DeSegMT
-python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_50_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_50_0.1_0/
 
 
 
 #SegPL_softmax
-python3 test_script.py --load_path ./saved_models/FT_SegPL_U_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_SegPL_U_50_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_SegPL_U_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_SegPL_U_50_0.1_0/
 
 
 #DeSegPL_softmax
-python3 test_script.py --load_path ./saved_models/FT_DeSegPL_U_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_DeSegPL_U_50_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_DeSegPL_U_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_DeSegPL_U_50_0.1_0/
 
 #SegMT_softmax
 python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_U_200_0.1_0/
