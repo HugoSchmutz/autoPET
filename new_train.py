d24e3000ccf1116cadf752ce2c21f4ab34886179
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-10-30 12:32:38 +0100

    test losses

--- new_train.py
+++ new_train.py
@@ -413,7 +413,7 @@ if __name__ == "__main__":
     '''
     Optimizer configurations
     '''
-    parser.add_argument('--learning_rate', type=float, default=1e-4)
+    parser.add_argument('--learning_rate', type=float, default=5e-4)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight_decay', type=float, default=1e-4)
     parser.add_argument('--amp', action='store_true', help='use mixed precision training or not')
