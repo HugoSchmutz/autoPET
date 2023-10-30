fe6de2224c0f14192fb505b302618c2ed1f86c41
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-10-30 11:17:47 +0100

    test losses

--- test_file.sh
+++ test_file.sh
@@ -3,7 +3,7 @@
 #python3 test_script.py --load_path ./saved_models/CC_50_pretraining/
 
 #CC
-python3 test_script.py --load_path ./saved_models/FT_CC_200_0.0_0/
+#python3 test_script.py --load_path ./saved_models/FT_CC_200_0.0_0/
 #python3 test_script.py --load_path ./saved_models/FT_CC_50_0.0_0/
 
 #SegPL
@@ -35,9 +35,23 @@ python3 test_script.py --load_path ./saved_models/FT_CC_200_0.0_0/
 #python3 test_script.py --load_path ./saved_models/FT_DeSegPL_U_50_0.1_0/
 
 #SegMT_softmax
-python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_U_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_U_50_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_U_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_MT_SegPL_U_50_0.1_0/
+#
+##DeSegMT_softmax
+#python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_200_0.1_0/
+#python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_50_0.1_0/
 
-#DeSegMT_softmax
-python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_200_0.1_0/
-python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_50_0.1_0/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.001/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.0001/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_5e-05/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_1e-05/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.001/
+
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.001/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.0001/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_5e-05/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_1e-05/
+python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.001/
