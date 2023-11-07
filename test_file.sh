7b6b962cf3f391dd7c0416a20e6f7847f400329a
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-11-07 12:18:56 +0100

    test losses

--- test_file.sh
+++ test_file.sh
@@ -42,16 +42,31 @@
 #python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_200_0.1_0/
 #python3 test_script.py --load_path ./saved_models/FT_DeMT_SegPL_U_50_0.1_0/
 
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.001/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.0005/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.0001/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_5e-05/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_1e-05/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.001/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.001/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.0005/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.0001/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_5e-05/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_1e-05/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_200_0.05_0_0.2_0.001/
+#
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.001/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.0005/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.0001/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_5e-05/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_1e-05/
+#python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.001/
 
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.001/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.0005/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.0001/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_5e-05/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_1e-05/
-python3 test_script.py --load_path ./saved_models/FT_CC_normal_CE_50_0.05_0_0.2_0.001/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_200_0.001_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_200_0.005_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_200_0.01_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_200_0.05_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_200_0.1_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_200_0.5_0_0.2_0.0005/
+
+
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_50_0.001_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_50_0.005_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_50_0.01_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_50_0.05_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_50_0.1_0_0.2_0.0005/
+python3 test_script.py --load_path ./saved_models/FT_PL_normal_DiceCE_50_0.5_0_0.2_0.0005/
