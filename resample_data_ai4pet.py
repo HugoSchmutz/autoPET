01b266500fbcf80903e93eebbe48505a68506546
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-11-27 16:50:58 +0100

    fix bug for resample data

--- resample_data_ai4pet.py
+++ resample_data_ai4pet.py
@@ -38,36 +38,39 @@ def resample_images(path_to_data, patients_list, studies_list, nii_out_root):
         os.makedirs(patient_dir_out, exist_ok=True)
 
     for study in tqdm(studies_list):
-        
-        patient, date = study.split('_')[0], study.split('_')[1]
-        patient_dir_out = os.path.join(nii_out_root, patient)
-        
-        study_dir_out = os.path.join(patient_dir_out, date)
-        os.makedirs(study_dir_out, exist_ok=False)
+        try:
+            patient, date = study.split('_')[0], study.split('_')[1]
+            patient_dir_out = os.path.join(nii_out_root, patient)
+            
+            study_dir_out = os.path.join(patient_dir_out, date)
+            os.makedirs(study_dir_out, exist_ok=False)
 
-        print("The following patient directory is being processed: ", patient, date)
-        
-        CT = nib.load(os.path.join(os.path.join(path_to_data, 'CT'),study + '.nii.gz'))
-        PET = nib.load(os.path.join(os.path.join(path_to_data, 'PT'),study + '.nii.gz'))
-        
-        SEG_list = os.listdir(os.path.join(os.path.join(path_to_data, 'SEG'),study))
-        
-        #Resample CT
-        CTres = nilearn.image.resample_to_img(CT, PET, fill_value=-1024)
-        
-        #Aggregate and resample segmentations:        
-        new_data = np.zeros(PET.get_fdata().shape) 
-        for seg in SEG_list:
-            SEG = nib.load(os.path.join(os.path.join(os.path.join(path_to_data, 'SEG'),study, seg)))
-            SEGres = nilearn.image.resample_to_img(SEG, PET, fill_value=-1024)
-            SEG_data = (np.copy(SEGres.get_fdata())<0.5).astype(np.int16)
-            new_data = new_data + SEG_data
-        
-        new_SEG = nib.Nifti1Image(new_data, SEGres.affine, header=SEGres.header)
+            print("The following patient directory is being processed: ", patient, date)
+            
+            CT = nib.load(os.path.join(os.path.join(path_to_data, 'CT'),study + '.nii.gz'))
+            PET = nib.load(os.path.join(os.path.join(path_to_data, 'PT'),study + '.nii.gz'))
+            
+            SEG_list = os.listdir(os.path.join(os.path.join(path_to_data, 'SEG'),study))
+            
+            #Resample CT
+            CTres = nilearn.image.resample_to_img(CT, PET, fill_value=-1024)
+            
+            #Aggregate and resample segmentations:        
+            new_data = np.zeros(PET.get_fdata().shape) 
+            for seg in SEG_list:
+                SEG = nib.load(os.path.join(os.path.join(os.path.join(path_to_data, 'SEG'),study, seg)))
+                SEGres = nilearn.image.resample_to_img(SEG, PET, fill_value=-1024)
+                SEG_data = nilearn.image.binarize_img(np.copy(SEGres.get_fdata())<0.5).astype(np.int16)
+                new_data = new_data + SEG_data
+            
+            new_SEG = nib.Nifti1Image(new_data, SEGres.affine, header=SEGres.header)
 
-        nib.save(CTres, os.path.join(study_dir_out,'CTres.nii.gz'))
-        nib.save(PET, os.path.join(study_dir_out,'PET.nii.gz'))
-        nib.save(new_SEG, os.path.join(study_dir_out,'SEG.nii.gz'))
+            nib.save(CTres, os.path.join(study_dir_out,'CTres.nii.gz'))
+            nib.save(PET, os.path.join(study_dir_out,'PET.nii.gz'))
+            nib.save(new_SEG, os.path.join(study_dir_out,'SEG.nii.gz'))
+        except:
+            print('PB with ', study)
+            pass
         
 
 
