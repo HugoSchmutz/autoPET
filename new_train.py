74ce32d94f800bf9907154f487330f6becf03053
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-11-29 15:12:35 +0100

    fix bug for resample data

--- new_train.py
+++ new_train.py
@@ -294,12 +294,18 @@ def main_worker(gpu, ngpus_per_node, args):
     patch_size = (args.patch_d0, args.patch_d1, args.patch_d2)
     #sampler = tio.data.UniformSampler(patch_size)
     
-    probabilities = {0: 0.5, 1: 0.5}
-    sampler = tio.data.LabelSampler(
-        patch_size=patch_size,
-        label_name='segmentation',
-        label_probabilities=probabilities,
+    # probabilities = {0: 0.5, 1: 0.5}
+    # sampler = tio.data.LabelSampler(
+    #     patch_size=patch_size,
+    #     label_name='segmentation',
+    #     label_probabilities=probabilities,
+    # )
+    
+    sampler = tio.data.UniformSampler(
+        patch_size=patch_size
     )
+    
+    
     lb_dset, ulb_dset, eval_dset = get_ssl_dataset(args.data_dir, 
                                                    args.patients_list_dir,
                                                    args.num_labels,
@@ -369,7 +375,7 @@ if __name__ == "__main__":
     '''
     Saving & loading of the model.
     '''
-    parser.add_argument('--save_dir', type=str, default='./new_saved_models')
+    parser.add_argument('--save_dir', type=str, default='./saved_models')
     parser.add_argument('--save_name', type=str, default='completecase')
     parser.add_argument('--resume', action='store_true')
     parser.add_argument('--finetune', action='store_true')
